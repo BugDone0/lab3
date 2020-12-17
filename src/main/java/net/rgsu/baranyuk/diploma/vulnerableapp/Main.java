d49ae66fccb43fb7d7489aaf6ba71e3296968bbd
pctf <baranukis@gmail.com>
2020-12-17 21:50:13 +0300

    Initial commit

---
+++ src/main/java/net/rgsu/baranyuk/diploma/vulnerableapp/Main.java
@@ -0,0 +1,7 @@
+package net.rgsu.baranyuk.diploma.vulnerableapp;
+
+public class Main {
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/test/java/net/rgsu/baranyuk/diploma/vulnerableapp/MainTest.java
@@ -0,0 +1,11 @@
+package net.rgsu.baranyuk.diploma.vulnerableapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void loads() {
+        Assertions.assertTrue(true, "should be true");
+    }
+}
